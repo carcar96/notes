6af1abcc05602e207d2fb4f32c06bcdc644bbbbf
carcar96 <chencaixia2018@163.com>
2018-03-04 17:44:48 +0800

    运算符

---
+++ python/02/运算符.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+"""
+比较（关系）运算符
+    1/  ==
+    2/  !=
+    3/  <>  等价于!=
+    4/  >
+    5/  >=
+    6/  <
+    7/  <=
+逻辑运算符
+    1/  and
+    2/  or
+    3/  not 不是某个值或某段范围
+"""
+age = 18
+print (age==18)
+print (age==8)
+print(not(age==18)) #false
+print(not(age>18))  #true
