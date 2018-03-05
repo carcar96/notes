984591b4453bd7963a1ed60066ff4d2b139a2844
carcar96 <chencaixia2018@163.com>
2018-03-05 13:37:46 +0800

    更新运算符

--- python/02/运算符.py
+++ python/02/运算符.py
@@ -9,9 +9,9 @@
     6/  <
     7/  <=
 逻辑运算符
-    1/  and
-    2/  or
-    3/  not 不是某个值或某段范围
+    1/  and（ 如果 a 为 false，a and b 返回 false，否则它返回 b 的计算值）
+    2/  or（ 如果 a 是非 0，它返回 a 的值，否则它返回 b 的计算值）
+    3/  not 不是某个值或某段范围，返回false或者true
 """
 age = 18
 print (age==18)
