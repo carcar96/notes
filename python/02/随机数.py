a19abcf2cdddb74cafe94a6ae2c8fb4cf3d7aa64
carcar96 <chencaixia2018@163.com>
2018-03-05 20:27:11 +0800

    随机数

--- python/02/随机数.py
+++ python/02/随机数.py
@@ -17,11 +17,13 @@ print random.randint(0,2)
 # random.seed(3)#0.237964627092
 # random.seed(4)#0.236048089737
 # random.seed(5)#0.62290169489
-print random.random()
-
+print random.random() #0.717435442568
+# random.seed(10)
+# print random.random()   #0.57140259469  同一个种子值，产生的随机数相同
+# print random.random()   #0.428889054675
 #   3. random.uniform(u,sigma)
 #       随机正态浮点数
-print random.uniform(1,5)
+print random.uniform(1,5) #3.21561360405
 
 #   4.random.randrange(start,stop,step)
 #       按步长随机在上下限范围内取一个随机数
@@ -29,15 +31,15 @@ print random.randrange(20,100,5)
 
 #   5. random.random()
 #       随机浮点数
-print random.random()
+print random.random()#0.821716538852
 
-#   6.random.shuffle
+#   6.random.shuffle()
 #       对list列表随机打乱顺序，也就是洗牌
 #       shuffle只作用于list，对str会报错比如‘abcdfed’,而['1','2','3','5','6','7']可以
 item = ['1','2','3','5','6','7']
-print item
+print item  #['1', '2', '3', '5', '6', '7']
 random.shuffle(item)
-print item
+print item  #['1', '2', '6', '7', '5', '3']
 
 #   7. 随机选择字符
 #   (1)random.choice(str)
@@ -46,8 +48,8 @@ print item
 #        随机选择几位字符,以数组形式返回 ['b', 'o', 'a']
 
 str = "abcn bosdh"
-print random.choice(str)
-print random.sample(str,3)
+print random.choice(str)    #'c'
+print random.sample(str,3)  #['h', 'b', 'c']
 
 #将获取到的随机字符串数组拼接成新字符串
 tarArr = random.sample(str,4)
