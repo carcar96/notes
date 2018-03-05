a7f07b1b9fc814714d3adf3c89a72b74b886dcc5
carcar96 <chencaixia2018@163.com>
2018-03-05 12:17:22 +0800

    打印三角形

---
+++ python/02/打印三角形.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+a = 4
+i = 1
+while i<=a:
+    print "*"*i,
+    print ""
+    i = i+1
--- python/02/打印矩形.py
+++ python/02/打印矩形.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
-a = 5 #width
-b = 6 #height
+a = 5 #矩形的宽
+b = 6 #矩形的高
 i = 1
 while i<=b:
     print "*"*a,
