e3224ab48632ae5976f859fc193e9fcfcaf6ef55
carcar96 <chencaixia2018@163.com>
2018-03-05 12:40:17 +0800

    优化九九乘法表

--- python/01/九九乘法表.py
+++ python/01/九九乘法表.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
 for i in range(1,10):
-    for j in range(1,10):
-        if j <= i :
-            print j,"*",i,"=",j*i," ",
+    for j in range(1,i+1):
+        print j,"*",i,"=",j*i,"\t", #\t table对齐
     print("")
